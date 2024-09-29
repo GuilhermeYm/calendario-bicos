5976b21fd77a6ba110c05639ea27092cb95d4799
Guilherme Ym <guilhermeym2009@gmail.com>
2024-09-29 00:21:14 -0300

    Arrumando algumas coisas

--- src/app/components/MainComponent.jsx
+++ src/app/components/MainComponent.jsx
@@ -1,9 +1,57 @@
+"use client";
+
 import React from "react";
 import "./style/MainComponentStyle.css";
 import { SlCalender } from "react-icons/sl";
 import { GrFormNextLink, GrFormPreviousLink } from "react-icons/gr";
+import { useEffect, useState } from "react";
+import useDays from "../hooks/useDays";
 
 const MainComponent = () => {
+  const { days, totalDays } = useDays();
+  const [day, setDay] = useState("");
+  const [month, setMonth] = useState("");
+  const [year, setYear] = useState("");
+  const [ totalDaysMain, setTotalDaysMain ] = useState(0);
+
+  const indentifyMonth = (month) => {
+    switch (month) {
+      case 1:
+        return "Janeiro";
+      case 2:
+        return "Fevereiro";
+      case 3:
+        return "Março";
+      case 4:
+        return "Abril";
+      case 5:
+        return "Maio";
+      case 6:
+        return "Junho";
+      case 7:
+        return "Julho";
+      case 8:
+        return "Agosto";
+      case 9:
+        return "Setembro";
+      case 10:
+        return "Outubro";
+      case 11:
+        return "Novembro";
+      case 12:
+        return "Dezembro";
+      default:
+        return "Mês não identificado";
+    }
+  }
+
+  useEffect(() => {
+    setDay(days.dia);
+    setMonth(indentifyMonth(days.mes));
+    setYear(days.ano);
+    setTotalDaysMain(totalDays);
+  }, [days]);
+
   return (
     <>
       <h1 className="titleMainContainer">
@@ -17,7 +65,11 @@ const MainComponent = () => {
           </button>
           <h2 className="nameMonth">
             <SlCalender />
-            <div className="nameMonth-space">Janeiro</div>
+            <div className="nameMonth-space">{month}</div>
+            <div className="styleMonth">
+                <i className="inline-block">de</i>
+                <p className="inline-block">{year}</p>
+            </div>
           </h2>
           <button className="nextMonth buttonMain">
             <GrFormNextLink />
--- src/app/hooks/useDays.jsx
+++ src/app/hooks/useDays.jsx
@@ -1,7 +1,57 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const useDays = () => {
-    console.log('Ainda em desenvolvimento')
+  const [days, setDaysState] = useState({});
+  const [totalDays, setTotalDays] = useState(0);
+
+  useEffect(() => {
+    // Função para retornar a data atual
+    const setDays = () => {
+      const date = new Date();
+      const day = date.getDate();
+      const month = date.getMonth() + 1;
+      const year = date.getFullYear();
+
+      // Setando
+      setDaysState({
+        dia: day,
+        mes: month,
+        ano: year,
+      });
+    };
+    // Chamar a função
+    setDays();
+  }, []); // Executa só uma vez
+
+  useEffect(() => {
+    if (days.ano && days.mes) {
+      // Função para pegar a quantidade de dias no mês
+      const getDayInMonth = (year, month) => {
+        return new Date(year, month, 0).getDate();
+      };
+      //Função para retornar a quantidade de dias no mês
+      const setTotalDaysFunction = (year, month) => {
+        console.log('Ano e mês recebido:', year, month);
+        const daysInaMaht = getDayInMonth(year, month);
+        console.log('Quantidade de dias:', daysInaMaht);
+
+        //Criar um array
+        const daysArray = Array.from({ length: daysInaMaht }, (_, i) => i + 1);
+        console.log(daysArray);
+        setTotalDays(daysArray);
+      };
+      setTotalDaysFunction(days.ano, days.mes); 
+    }
+  }, [days]);
+
+//   const getFirstSevenDays = () => { 
+//     if (days.)
+//   }
+
+  return {
+    days,
+    totalDays,
+  };
 };
 
 export default useDays;
